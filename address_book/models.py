98fe9657d325e05540b9e6c4215397f3ca2028f6
dmytrorevak <dmytro.revak@gmail.com>
2017-07-25 14:57:17 +0300

    add simply login logic

--- address_book/models.py
+++ address_book/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Contact(models.Model):
+
+    user = models.ManyToManyField(User)
+    name = models.CharField(max_length=25)
+    surname = models.CharField(max_length=30)
+    age = models.PositiveIntegerField() 
+    phone_number = models.IntegerField()
+    # avatar = models.ImageField(upload_to='avatars/')
+    address = models.CharField(max_length=100)
+    description = models.TextField()
--- address_book/views.py
+++ address_book/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import auth
 
 
 def contacts_list_view(request):
-
+    print(request.user)
     if request.user.is_authenticated():
         return render(request, 'address_book/index.html',
                       {'user': request.user})
     else:
         return render(request, 'address_book/err.html')
+
+
+def login_view(request):
+    return render(request, 'address_book/login.html')
+
+
+def auth_view(request):
+    user_name = request.POST['login']
+    print(user_name)
+    user_password = request.POST['password']
+    print(user_password)
+    user = auth.authenticate(username=user_name, password=user_password)
+    print(user)
+    if user is not None:
+        auth.login(request, user)
+
+    return HttpResponseRedirect('/')
+
+    # else:
+    # return HttpResponseRedirect('address_book/err')
